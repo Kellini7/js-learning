7ec195870b497d9677f8a9508fba52a6e150b55c
Kellini7 <118812258+Kellini7@users.noreply.github.com>
2023-12-24 04:14:14 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 141414
+let accountEmail ="ke@gmail.com"
+var accountPassword = "12345"
+
+console.table([accountId, accountEmail, accountPassword]);
+
+/* Don't use var because of a issue in scope */
+
